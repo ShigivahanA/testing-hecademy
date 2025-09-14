2d8fbf9da242d80312ef0d47ddc6684bb39ccd5a
ShigivahanA <shigivahan@gmail.com>
2025-09-14 11:49:33 +0530

    update server.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,6 +11,6 @@ userRouter.get('/enrolled-courses', userEnrolledCourses)
 userRouter.post('/update-course-progress', updateUserCourseProgress)
 userRouter.post('/get-course-progress', getUserCourseProgress)
 userRouter.post('/add-rating', addUserRating)
-router.put("/preferences", requireAuth(), updateUserPreferences)
+userRouter.put("/preferences", requireAuth(), updateUserPreferences)
 
 export default userRouter;

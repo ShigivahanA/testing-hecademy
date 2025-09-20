c9eed18138254db3c40c8178a05c3541387da6fe
ShigivahanA <shigivahan@gmail.com>
2025-09-20 18:42:29 +0530

    card updated

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -10,9 +10,9 @@ const CourseCard = ({ course }) => {
     return (
         <Link onClick={() => scrollTo(0, 0)} to={'/course/' + course._id} className="border border-gray-500/30 pb-6 overflow-hidden rounded-xl hover:bg-gradient-to-b from-cyan-100/70 shadow-md transform transition-transform duration-300 hover:scale-105 ">
             <img className="w-full" src={course.courseThumbnail} alt='' />
-            <div className="p-3 text-left">
+            <div className="p-3 text-left space-y-2">
                 <h3 className="text-base font-semibold">{course.courseTitle}</h3>
-                <p className="text-gray-500">{course.educator.name}</p>
+                {/* <p className="text-gray-500">{course.educator.name}</p> */}
                 <div className="flex items-center space-x-2">
                     <p>{calculateRating(course)}</p>
                     <div className="flex">

f60070e9328855b94b399fefc2a4c91fb9fb3b66
ShigivahanA <shigivahan@gmail.com>
2025-10-01 16:48:31 +0530

    update server

--- server/models/Certificates.js
+++ server/models/Certificates.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
 const CertificateSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
-  certificateId: { type: String, unique: true, required: true },
+  userId: {
+        type: String,
+        ref: 'User',
+        required: true
+    },
+    courseId: { type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+        required: true
+    },
+      certificateId: { type: String, unique: true, required: true },
   issueDate: { type: Date, default: Date.now },
 }, { timestamps: true });
 
